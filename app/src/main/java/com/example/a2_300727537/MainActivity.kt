3067850c8e5da19ce389d34f3efef33c32bab1a3
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-20 16:30:14 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/a2_300727537/MainActivity.kt
+++ app/src/main/java/com/example/a2_300727537/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.a2_300727537
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.a2_300727537.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.btnEnter.setOnClickListener {
+            val intent = Intent(this, HomeTypeActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
