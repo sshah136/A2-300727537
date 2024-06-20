091feee9f7f25e5c615593d9e3ead28bd38ddb91
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-20 16:30:11 -0400

    Create ListingActivity.kt

---
+++ app/src/main/java/com/example/a2_300727537/ListingActivity.kt
@@ -0,0 +1,30 @@
+package com.example.a2_300727537
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.a2_300727537.databinding.ActivityListingBinding
+import com.example.a2_300727537.model.Listing
+
+class ListingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityListingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityListingBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val listings = arrayListOf<Listing>()
+        for (i in 0..100) {
+            listings.add(Listing("Building Listing $i", "Description $i",1.11,""))
+
+        }
+
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@ListingActivity)
+            adapter = ListingAdapter(listings)
+        }
+    }
+
+}
