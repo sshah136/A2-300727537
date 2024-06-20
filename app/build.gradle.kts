8a38d53d8f8cf5548671aff2a36a4897d8db6bcd
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-20 17:55:24 -0400

    import picasso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.squareup.picasso:picasso:2.71828")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
