50bc85c96cc32d4624dbf8f1f687f32404628d6d
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-06-20 16:30:05 -0400

    Update HomeTypeActivity.kt

--- app/src/main/java/com/example/a2_300727537/HomeTypeActivity.kt
+++ app/src/main/java/com/example/a2_300727537/HomeTypeActivity.kt
@@ -1,6 +1,10 @@
 package com.example.a2_300727537
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,12 +12,24 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.a2_300727537.databinding.ActivityHomeTypeBinding
 
 class HomeTypeActivity : AppCompatActivity() {
-    lateinit var binding: ActivityHomeTypeBinding
+    //lateinit var binding: ActivityHomeTypeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeTypeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_home_type)
+//        binding = ActivityHomeTypeBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        setSupportActionBar(binding.toolbar)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.miHome -> startActivity(Intent(this, ListingActivity::class.java))
+        }
+        return true
     }
 }
